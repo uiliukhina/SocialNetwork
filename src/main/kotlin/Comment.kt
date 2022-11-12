c64b82958c65bcbd62701e5068bfc40d27a5a23a
Yulia Ilyukhina <uilluhina@icloud.com>
2022-11-12 19:16:43 +0300

    added Comment.kt

---
+++ src/main/kotlin/Comment.kt
@@ -0,0 +1,13 @@
+class Comment(
+    count: Int = 0, //  количество комментариев
+    canPost: Boolean = true,
+    groupsCanPost: Boolean = true,
+    canClose: Boolean, //   может ли текущий пользователь закрыть комментарии к записи
+    canOpen: Boolean
+) {
+}
+
+
+
+
+
--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,17 +1,15 @@
-class Post(
-    id: Int,//  Идентификатор записи.
-    ownerId: Int,// Идентификатор владельца стены, на которой размещена запись.
-    fromId: Int,//  Идентификатор автора записи (от чьего имени опубликована запись).
-    date: Int,//    Время публикации записи в формате unixtime.
-    text: String,
-    friendsOnly: Boolean,
-    canPin: Boolean,
-    canDelete: Boolean,
-    canEdit: Boolean,
-    isPinned: Boolean,
-    markedAsAds: Boolean,
-    isFavorite: Boolean//   true, если объект добавлен в закладки у текущего пользователя
-) {
-
-}
+data class Post(
+    val id: Int,//  Идентификатор записи.
+    val ownerId: Int,// Идентификатор владельца стены, на которой размещена запись.
+    val fromId: Int,//  Идентификатор автора записи (от чьего имени опубликована запись).
+    val date: Int,//    Время публикации записи в формате unixtime.
+    val text: String,
+    val friendsOnly: Boolean,
+    val canPin: Boolean,
+    val canDelete: Boolean,
+    val canEdit: Boolean,
+    val isPinned: Boolean,
+    val markedAsAds: Boolean,
+    val isFavorite: Boolean//   true, если объект добавлен в закладки у текущего пользователя
+)
 
