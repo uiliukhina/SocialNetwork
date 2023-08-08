d92b03bb71491cb6706aeea5777359c6572a0ba4
Yulia Ilyukhina <uilluhina@icloud.com>
2023-08-08 12:57:53 +0300

    commited Post changes(added all fields)

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -2,15 +2,30 @@ data class Post(
     var id: Int = 0,//  Идентификатор записи.
     val ownerId: Int,// Идентификатор владельца стены, на которой размещена запись.
     val fromId: Int,//  Идентификатор автора записи (от чьего имени опубликована запись).
+    val createdBy:Int,// Идентификатор администратора, который опубликовал запись
     val date: Int,//    Время публикации записи в формате unixtime.
     val text: String,
+    val replyOwnerId: Int, // Идентификатор владельца записи, в ответ на которую была оставлена текущая
+    val replyPostId: Int, // Идентификатор записи, в ответ на которую была оставлена текущая
     val friendsOnly: Boolean = true,
+    val copyright: Copyright,
+    val likes: Likes,
+    val reposts: Reposts,
+    val views: Views,
+    val postType: String,
+    val postSource: PostSource,
+    val geo: Geo,
+    val signerId: Int,
+    val copyHistory: Int[],
     val canPin: Boolean = true,
     val canDelete: Boolean= true,
     val canEdit: Boolean= true,
     val isPinned: Boolean= false,
     val markedAsAds: Boolean= false,
     val isFavorite: Boolean = false,//   true, если объект добавлен в закладки у текущего пользователя
+    val donut: Donut,
+    val postponedId: Int,
     val comment: Comment
+
     )
 
